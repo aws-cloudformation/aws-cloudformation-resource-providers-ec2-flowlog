2dfd21672056b93bbfd9c51cc1752b616e07ab7c
Rafael Setra <16005314+rsetra@users.noreply.github.com>
2020-04-29 17:59:01 -0400

    Add files via upload

---
+++ aws-ec2-flowlog/src/main/java/software/amazon/ec2/flowlog/ListHandler.java
@@ -0,0 +1,56 @@
+package software.amazon.ec2.flowlog;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import software.amazon.awssdk.services.ec2.model.DescribeFlowLogsRequest;
+import software.amazon.awssdk.services.ec2.model.DescribeFlowLogsResponse;
+import software.amazon.awssdk.services.ec2.model.Ec2Exception;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+
+public class ListHandler extends BaseHandler<CallbackContext> {
+
+    @Override
+    public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+        final AmazonWebServicesClientProxy proxy,
+        final ResourceHandlerRequest<ResourceModel> request,
+        final CallbackContext callbackContext,
+        final Logger logger) {
+
+        try {
+            final DescribeFlowLogsResponse describeFlowLogsResponse =
+                proxy.injectCredentialsAndInvokeV2(createListRequest(request.getNextToken()),
+                    ClientBuilder.getClient()::describeFlowLogs);
+
+            logger.log(String.format("%n %s listed successfully",
+                    describeFlowLogsResponse.flowLogs().size(), ResourceModel.TYPE_NAME));
+
+            return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                .status(OperationStatus.SUCCESS)
+                .resourceModels(createListResourceModels(describeFlowLogsResponse))
+                .nextToken(describeFlowLogsResponse.nextToken())
+                .build();
+        } catch (Ec2Exception e) {
+            return ProgressEvent.defaultFailureHandler(e,
+                    Translator.getHandlerErrorForEc2Error(e.awsErrorDetails().errorCode()));
+        }
+    }
+
+    private DescribeFlowLogsRequest createListRequest(final String nextToken) {
+        return DescribeFlowLogsRequest.builder()
+            .maxResults(500)
+            .nextToken(nextToken)
+            .build();
+    }
+
+    private List<ResourceModel> createListResourceModels(final DescribeFlowLogsResponse response) {
+        return response.flowLogs()
+            .stream()
+            .map(flowLog -> Translator.createModelFromFlowLog(flowLog))
+            .collect(Collectors.toList());
+    }
+}
---
+++ aws-ec2-flowlog/src/test/java/software/amazon/ec2/flowlog/CreateHandlerTest.java
@@ -0,0 +1,174 @@
+package software.amazon.ec2.flowlog;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import software.amazon.awssdk.services.ec2.model.CreateFlowLogsResponse;
+import software.amazon.awssdk.services.ec2.model.UnsuccessfulItem;
+import software.amazon.awssdk.services.ec2.model.UnsuccessfulItemError;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.HandlerErrorCode;
+import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateHandlerTest extends TestBase {
+
+    @Mock
+    private AmazonWebServicesClientProxy proxy;
+
+    @Mock
+    private Logger logger;
+
+    @BeforeEach
+    public void setup() {
+        proxy = mock(AmazonWebServicesClientProxy.class);
+        logger = mock(Logger.class);
+    }
+
+    @Test
+    public void handleRequest_SimpleSuccess() {
+        final CreateHandler handler = new CreateHandler();
+
+        final CreateFlowLogsResponse createFlowLogsResponse = CreateFlowLogsResponse.builder()
+            .unsuccessful(Collections.emptyList())
+            .flowLogIds("fl-0")
+            .build();
+
+        doReturn(createFlowLogsResponse)
+            .when(proxy)
+            .injectCredentialsAndInvokeV2(
+                ArgumentMatchers.any(),
+                ArgumentMatchers.any()
+            );
+
+        final ResourceModel model = ResourceModel.builder().build();
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(model)
+            .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, null, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackContext()).isNull();
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModel()).isEqualTo(model);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isNull();
+        assertThat(response.getNextToken()).isNull();
+    }
+
+    @Test
+    public void handleRequest_Unsuccessful() {
+        final CreateHandler handler = new CreateHandler();
+
+        final CreateFlowLogsResponse createFlowLogsResponse = CreateFlowLogsResponse.builder()
+            .unsuccessful(UnsuccessfulItem.builder()
+                .error(UnsuccessfulItemError.builder()
+                    .code("InternalError")
+                    .message("InternalError")
+                    .build())
+                .build())
+            .build();
+
+        doReturn(createFlowLogsResponse)
+            .when(proxy)
+            .injectCredentialsAndInvokeV2(
+                ArgumentMatchers.any(),
+                ArgumentMatchers.any()
+            );
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(ResourceModel.builder().build())
+            .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, null, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackContext()).isNull();
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModel()).isNull();
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).isNotNull();
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.GeneralServiceException);
+        assertThat(response.getNextToken()).isNull();
+    }
+
+    @Test
+    public void handleRequest_TooManyFlowLogIds() {
+        final CreateHandler handler = new CreateHandler();
+
+        final CreateFlowLogsResponse createFlowLogsResponse = CreateFlowLogsResponse.builder()
+            .unsuccessful(Collections.emptyList())
+            .flowLogIds("fl-0", "fl-1")
+            .build();
+
+        doReturn(createFlowLogsResponse)
+            .when(proxy)
+            .injectCredentialsAndInvokeV2(
+                ArgumentMatchers.any(),
+                ArgumentMatchers.any()
+            );
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(ResourceModel.builder().build())
+            .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, null, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackContext()).isNull();
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModel()).isNull();
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).isNotNull();
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InternalFailure);
+        assertThat(response.getNextToken()).isNull();
+    }
+
+    @Test
+    public void handleRequest_SimpleError() {
+        final CreateHandler handler = new CreateHandler();
+
+        doThrow(SERVICE_UNAVAILABLE_ERROR)
+            .when(proxy)
+            .injectCredentialsAndInvokeV2(
+                ArgumentMatchers.any(),
+                ArgumentMatchers.any()
+            );
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(ResourceModel.builder().build())
+            .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, null, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackContext()).isNull();
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModel()).isNull();
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).isNotNull();
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.ServiceInternalError);
+        assertThat(response.getNextToken()).isNull();
+    }
+
+}
